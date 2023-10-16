fa5cfa6b5a5bd9ea3b6deba7268fcacc333411e7
SandroAma <50810024+sandroama@users.noreply.github.com>
2023-10-15 22:25:01 -0400

    market Cap formatting

--- Backend/app.py
+++ Backend/app.py
@@ -49,6 +49,22 @@ def get_stock_data():
 
     'trailingPE': 36.634457,
     '''
+    def format_market_cap(value):
+        if value == "N/A":
+            return value
+
+        trillion = 1_000_000_000_000
+        billion = 1_000_000_000
+        million = 1_000_000
+
+        if value >= trillion:
+            return f"{value / trillion:.3f}T"
+        elif value >= billion:
+            return f"{value / billion:.3f}B"
+        elif value >= million:
+            return f"{value / million:.3f}M"
+        else:
+            return f"{value}"
 
     # ALL FETCHING DOWN BELOW
     current_price = data['Close'].iloc[-1].round(2) if not data.empty else "N/A"
