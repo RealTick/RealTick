1e42297c990b7f46c94cb58c9ebc9e9a07497a10
SandroAma <50810024+sandroama@users.noreply.github.com>
2023-10-15 22:40:04 -0400

    data fetching
    made calls to get full name of the company, formated name and symbol together

--- Backend/app.py
+++ Backend/app.py
@@ -23,19 +23,19 @@ def get_stock_data():
     chart_data = {date.strftime('%Y-%m-%d'): close for date, close in data['Close'].items()}
 
 
-    # # YEARLY RETURN AND YTD RETURN
-    # # Calculate yearly return
-    # if len(data) > 0:
-    #     yearly_return = ((data['Close'].iloc[-1] - data['Close'].iloc[0]) / data['Close'].iloc[0]) * 100
-    # else:
-    #     yearly_return = "N/A"
+    # YEARLY RETURN AND YTD RETURN
+    # Calculate yearly return
+    if len(data) > 0:
+        yearly_return = ((data['Close'].iloc[-1] - data['Close'].iloc[0]) / data['Close'].iloc[0]) * 100
+    else:
+        yearly_return = "N/A"
 
-    # # Calculate YTD return
-    # start_of_year = data[data.index >= f"{data.index[-1].year}-01-01"]
-    # if not start_of_year.empty:
-    #     ytd_return = ((data['Close'].iloc[-1] - start_of_year['Close'].iloc[0]) / start_of_year['Close'].iloc[0]) * 100
-    # else:
-    #     ytd_return = "N/A"
+    # Calculate YTD return
+    start_of_year = data[data.index >= f"{data.index[-1].year}-01-01"]
+    if not start_of_year.empty:
+        ytd_return = ((data['Close'].iloc[-1] - start_of_year['Close'].iloc[0]) / start_of_year['Close'].iloc[0]) * 100
+    else:
+        ytd_return = "N/A"
 
 
     ''' TODO: Includes asset statistics such as:
@@ -71,16 +71,24 @@ def get_stock_data():
     prev_close = stock.history(period="2d")['Close'].iloc[0].round(2) if len(stock.history(period="2d")) > 1 else "N/A"
     opening_price = data['Open'].iloc[-1].round(2) if not data.empty else "N/A"
     market_cap= format_market_cap(stock.info.get('marketCap',"N/A"))
-    volume = data['Volume'].iloc[-1].round(2) if not data.empty else "N/A"
+    volume = int(data['Volume'].iloc[-1].round(2)) if not data.empty else "N/A"
     fifty_two_week_range = (data['Low'].min().round(2), data['High'].max().round(2)) if not data.empty else ("N/A", "N/A")
     forward_dividend_yield = f"{stock.info.get('forwardPE', 'N/A')} x {stock.info.get('forwardEps', 'N/A')}" if stock.info.get('forwardPE') and stock.info.get('forwardEps') else "N/A"
     days_range = (data['Low'].iloc[-1].round(2), data['High'].iloc[-1].round(2)) if not data.empty else ("N/A", "N/A")
     beta = stock.info.get('beta', "N/A")
+    stock_long_name=stock.info.get('longName')
+    stock_short_name=stock.info.get('shortName')
+    stock_symbol=stock.info.get('symbol')
+    stock_display_name=stock_long_name+f'({stock_symbol})'
 
     if not data.empty:
         return jsonify({
+            'stock_display_name': stock_display_name,
+            'long_name': stock_long_name,
+            'short_name':stock_short_name,
+            'symbol': stock_symbol,
             'current_price': current_price,
-            'prev_close':prev_close,
+            'prev_close': prev_close,
             'opening_price': opening_price,
             'market_cap': market_cap,
             'volume': volume,
@@ -90,7 +98,6 @@ def get_stock_data():
             'beta': beta,
             'yearly_return': yearly_return,
             'ytd_return': ytd_return,
-            'market_cap': stock.info.get('marketCap', "N/A"),
             'chart': chart_data
         })
     else:
