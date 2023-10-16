42b353409906a1fdb6d54189a31859673d546c52
SandroAma <50810024+sandroama@users.noreply.github.com>
2023-10-16 11:15:21 -0400

    s1

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-import fetchData from './components/StockService';
-import StockInfo from './components/StockInfo';
-import ErrorMessage from './components/errorMessage'; 
-import Input from './components/Input';
-import { ThemeProvider } from './contexts/ThemeContext';
-import ThemeLoader from '../public/themes/ThemeLoader';
-import LineChart from './components/LineChart';
-import NewsModule from './components/NewsModule';
+import fetchData from "./components/StockService";
+import StockInfo from "./components/StockInfo";
+import ErrorMessage from "./components/errorMessage";
+import Input from "./components/Input";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import ThemeLoader from "../public/themes/ThemeLoader";
+import LineChart from "./components/LineChart";
+import NewsModule from "./components/NewsModule";
+import { Line } from "react-chartjs-2";
 
 function App() {
-  const [inputSymbol, setInputSymbol] = useState(''); 
-  const [displayedSymbol, setDisplayedSymbol] = useState('');
+  const [inputSymbol, setInputSymbol] = useState("");
+  const [displayedSymbol, setDisplayedSymbol] = useState("");
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
 
@@ -24,12 +25,12 @@ function App() {
       if (response) {
         setData(response);
         setError(null);
-        setDisplayedSymbol(inputSymbol);  // Update displayedSymbol only upon successful fetching
+        setDisplayedSymbol(inputSymbol); // Update displayedSymbol only upon successful fetching
       } else {
-        setError('Received unexpected data format.');
+        setError("Received unexpected data format.");
       }
     } catch (err) {
-      setError(`Error fetching data: ${err.message || 'Please try again.'}`);
+      setError(`Error fetching data: ${err.message || "Please try again."}`);
       setData(null);
     }
   };
@@ -37,44 +38,41 @@ function App() {
   return (
     <ThemeProvider>
       <ThemeLoader />
-     
+
       <div className="App">
-  
-  <div className='searchContainer'>
-    <div className='searchBox'>
-      <Input 
-        symbol={inputSymbol} 
-        setSymbol={setInputSymbol} 
-        fetchData={handleFetchData} 
-      />
-    </div>
-  </div>
-  
-  <div className="contentContainer">
-    <div className='stockDataContainer'>
-      <div className='stockData'>
-        {data && <StockInfo symbol={displayedSymbol} data={data} />}
+        <div className="searchContainer">
+          <div className="searchBox">
+            <Input
+              symbol={inputSymbol}
+              setSymbol={setInputSymbol}
+              fetchData={handleFetchData}
+            />
+          </div>
+        </div>
+
+        <div className="contentContainer">
+          <div className="stockDataContainer">
+            <div className="stockData">
+              {data && <StockInfo symbol={displayedSymbol} data={data} />}
+            </div>
+            <ErrorMessage error={error} />
+          </div>
+        </div>
+
+        {/* console.log(data?.chart_data)
+    <LineChart chartData={data?.chart_data}/> */}
+        {/* <div className="lineChartContainer">
+          <div className="LineChart">
+            <LineChart chartData={data?.chart} />
+          </div>
+        </div> */}
+
+        <div className="stockDataContainer">
+          <div className="newsModule">
+            <NewsModule data={data?.news} />
+          </div>
+        </div>
       </div>
-      <ErrorMessage error={error} />
-    </div>
-    
-    
-  </div>
-
-  <div className='lineChartContainer'>
-      <div className='LineChart'>
-        <LineChart chartData={data?.chart}/>
-      </div>
-    </div>
-
-  <div className='stockDataContainer'>
-    <div className='newsModule'>
-      <NewsModule data={data?.news} />
-    </div>
-  </div>
-  
-</div>
-
     </ThemeProvider>
   );
 }
