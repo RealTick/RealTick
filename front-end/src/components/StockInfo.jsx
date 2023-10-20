bd28c5cf6f453d320c48b54ec90a1714a80ffb28
zoo <30762520+zu-sye@users.noreply.github.com>
2023-10-20 17:49:31 -0400

    updated stock.info.css
    changed modularity of stock info component

--- front-end/src/components/StockInfo.jsx
+++ front-end/src/components/StockInfo.jsx
@@ -8,28 +8,54 @@ const StockInfo = ({ symbol, data }) => {
 
       <div className={styles.dataColumns}>
         <div className={styles.column}>
-          {/* TODO: This should be with the title and different CSS group so that it changes color on change of data */}
-          <p className={styles.dataPoint}>Current Price: {data.current_price}</p>
-          <p className={styles.dataPoint}>Previous Close: {data.prev_close}</p>
-          <p className={styles.dataPoint}>Market Cap: {data.market_cap}</p>
-          <p className={styles.dataPoint}>Open: {data.opening_price}</p>
-          <p className={styles.dataPoint}>Beta (5Y Monthly): {data.beta}</p>
-
+          <div className={styles.dataPoint}>
+            <span className={styles.dataTitle}>Current Price:</span>
+            <span className={styles.dataValue}>{data.current_price}</span>
+          </div>
+          <div className={styles.dataPoint}>
+            <span className={styles.dataTitle}>Previous Close:</span>
+            <span className={styles.dataValue}>{data.prev_close}</span>
+          </div>
+          <div className={styles.dataPoint}>
+            <span className={styles.dataTitle}>Market Cap:</span>
+            <span className={styles.dataValue}>{data.market_cap}</span>
+          </div>
+          <div className={styles.dataPoint}>
+            <span className={styles.dataTitle}>Open:</span>
+            <span className={styles.dataValue}>{data.opening_price}</span>
+          </div>
+          <div className={styles.dataPoint}>
+            <span className={styles.dataTitle}>Beta (5Y Monthly):</span>
+            <span className={styles.dataValue}>{data.beta}</span>
+          </div>
+          <div className={styles.dataPoint}>
+            <span className={styles.dataTitle}>Day's Range:</span>
+            <span className={styles.dataValue}>{data.days_range}</span>
+          </div>
         </div>
         <div className={styles.column}>
-          <p className={styles.dataPoint}>Day's Range: {data.days_range}</p>
-          <p className={styles.dataPoint}>52 Week Range: {data.fifty_two_week_range}</p>
-          <p className={styles.dataPoint}>Forward Dividend & Yield (FIX): {data.forward_dividend_yield}</p>
-          <p className={styles.dataPoint}>Volume: {data.volume}</p>
-          <p className={styles.dataPoint}>Yearly Return: {data.yearly_return}</p>
-          <p className={styles.dataPoint}>Year-To-Date Return: {data.ytd_return}</p>
+          <div className={styles.dataPoint}>
+            <span className={styles.dataTitle}>52 Week Range:</span>
+            <span className={styles.dataValue}>{data.fifty_two_week_range}</span>
+          </div>
+          <div className={styles.dataPoint}>
+            <span className={styles.dataTitle}>Forward Dividend & Yield (FIX):</span>
+            <span className={styles.dataValue}>{data.forward_dividend_yield}</span>
+          </div>
+          <div className={styles.dataPoint}>
+            <span className={styles.dataTitle}>Volume:</span>
+            <span className={styles.dataValue}>{data.volume}</span>
+          </div>
+          <div className={styles.dataPoint}>
+            <span className={styles.dataTitle}>Yearly Return:</span>
+            <span className={styles.dataValue}>{data.yearly_return}</span>
+          </div>
+          <div className={styles.dataPoint}>
+            <span className={styles.dataTitle}>Year-To-Date Return:</span>
+            <span className={styles.dataValue}>{data.ytd_return}</span>
+          </div>
         </div>
-
       </div>
-      
-      
-      {/* Additional data points go here */}
-      {/* Display the chart data (You can integrate Chart.js for better visualization later) */}
     </div>
   );
 };
