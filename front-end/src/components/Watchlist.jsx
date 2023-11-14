db1fdb5b4103f79806b82ec3639fb351d8f1bd30
LukeWyszynski <lwyszynski1@pride.hofstra.edu>
2023-11-14 17:24:45 -0500

    watchlist ui complete including pin button, working on functionality next

--- front-end/src/components/Watchlist.jsx
+++ front-end/src/components/Watchlist.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './component_css/Watchlist.module.css';
-import Input from './Input';
 
-const Watchlist = ({ handleFetchData }) => {
-    const tickers = [
-        'META',
-        'AAPL',
-        'AMZN',
-        'NVDA',
-        'NFLX',
-        'INTC'
-    ];
-      
+const Watchlist = ({ handleFetchData, currentTicker }) => {
+    const [watchlist, setWatchlist] = useState(() => {
+        // Load watchlist from local storage
+        return JSON.parse(localStorage.getItem('watchlist')) || [];
+    });
+
+    useEffect(() => {
+        // Save the watchlist to local storage whenever it changes
+        localStorage.setItem('watchlist', JSON.stringify(watchlist));
+    }, [watchlist]);
+
     const handleItemClick = (symbol) => {
-        console.log("Button clicked with symbol:", symbol);
+        console.log("Ticker clicked:", symbol);
         handleFetchData(symbol);
+    };
+
+    const handlePinClick = () => {
+        if (currentTicker && !watchlist.includes(currentTicker)) {
+            setWatchlist([...watchlist, currentTicker]);
+        }
     };
 
     return (
         <div className={styles.watchlistContainer}>
             <h2 className={styles.title}>{"Watchlist"}</h2>
+            <button className={styles.pinButton} onClick={handlePinClick}>
+                Pin Current
+            </button>
             <ul className={styles.listContainer}>
-                {tickers.map((item, index) => (
+                {watchlist.map((item, index) => (
                     <li key={index} className={styles.listItem}>
                         <button className={styles.invisibleButton} onClick={() => handleItemClick(item)}>
                             {item}
