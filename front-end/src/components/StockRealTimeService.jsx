5b03c8767d41fb66653ff0e2e55697a5b118044d
SandroAma <50810024+sandroama@users.noreply.github.com>
2023-11-23 21:24:25 -0500

    transfered changes

---
+++ front-end/src/components/StockRealTimeService.jsx
@@ -0,0 +1,16 @@
+// StockService.jsx
+import axios from "axios";
+
+// TODO: implement second url
+const fetchRealTimeData = async (symbol) => {
+  try {
+    const response = await axios.get(
+      `http://127.0.0.1:5000/realtime_stock?symbol=${symbol}`
+    );
+    return response.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export default fetchRealTimeData; 
